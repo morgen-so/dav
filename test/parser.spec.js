b7d54e75f382c4a25b8c210a30360a33efdc5a16
Marco Ancona <marco@morgen.so>
2023-05-17 14:53:09 +0200

    minor

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -3,14 +3,14 @@ const { multistatus, beautifyXML } = require('../lib/parser');
 const { getProps, mergeProps } = require('../lib/request');
 
 describe('Parser', function () {
-  test('beautifyXML should not destroy information', async function () {
+  test('beautifyXML should not destroy or add information', async function () {
     // Read xml from file
     let xml = fs.readFileSync('./test/unit/data/generic_xml.xml', 'utf8');
     const clean = beautifyXML(xml);
     expect(clean).toEqual(xml);
   });
 
-  test('beautifyXML should not destroy information (2)', async function () {
+  test('beautifyXML should not destroy or add information (2)', async function () {
     // Read xml from file
     let xml = fs.readFileSync('./test/unit/data/generic_xml_b.xml', 'utf8');
     const clean = beautifyXML(xml);
@@ -23,6 +23,9 @@ describe('Parser', function () {
       './test/unit/data/propfind_proxies_sogo.xml',
       'utf8'
     );
+
+    // The code below reproduces what is happening once the response is received.
+    // Unfortunately it is not easy to test the code directly.
     let responses = multistatus(xml).response.map((res) => {
       return {
         href: res.href,
